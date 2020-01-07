efc43fc8737f13b4803e0c37077f2079a58f6c71
Lukasz <lukasz.gras.97@gmail.com>
2020-01-07 15:24:22 +0100

    Updates

---
+++ precache-manifest.c00171d613d0794fdd730cfe04b8fa25.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53760a159418e03cb0707ac42e80248f",
+    "url": "/gym/index.html"
+  },
+  {
+    "revision": "2b64f6c3f4278e6cf0a0",
+    "url": "/gym/static/css/2.e10f3c82.chunk.css"
+  },
+  {
+    "revision": "2b64f6c3f4278e6cf0a0",
+    "url": "/gym/static/js/2.f6bd5b75.chunk.js"
+  },
+  {
+    "revision": "18c11b1aff9cd24ca116a502af5a7fe7",
+    "url": "/gym/static/js/2.f6bd5b75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "440b64e7aa820220288b",
+    "url": "/gym/static/js/main.26138fe5.chunk.js"
+  },
+  {
+    "revision": "da4bf9fab7ce97dfed59",
+    "url": "/gym/static/js/runtime-main.4f11fab4.js"
+  },
+  {
+    "revision": "29dca798360c56789155502277fafb24",
+    "url": "/gym/static/media/bg.29dca798.jpg"
+  },
+  {
+    "revision": "4ef667094004a0caf7621993380dd39a",
+    "url": "/gym/static/media/bg.4ef66709.jpg"
+  },
+  {
+    "revision": "560c8934c9ce2df8e617d62e23f1c5ed",
+    "url": "/gym/static/media/mot.560c8934.jpg"
+  },
+  {
+    "revision": "b07f3dd66376b959fec073474c42abbf",
+    "url": "/gym/static/media/rope.b07f3dd6.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/gym/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/gym/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/gym/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/gym/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "193a7ebe8ff34d40b6f9591a992ea295",
+    "url": "/gym/static/media/slider1.193a7ebe.jpg"
+  },
+  {
+    "revision": "e9f6ced5efc4d812a3df637501ff3056",
+    "url": "/gym/static/media/slider2.e9f6ced5.jpg"
+  },
+  {
+    "revision": "a160ccd536abdd6663ca58dd51ace1fe",
+    "url": "/gym/static/media/slider3.a160ccd5.jpg"
+  },
+  {
+    "revision": "ddac63757723241304afd5aa5c74d757",
+    "url": "/gym/static/media/training.ddac6375.jpg"
+  }
+]);
